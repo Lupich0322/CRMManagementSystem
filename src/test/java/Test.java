d0d88343cba737b57fe0eb3bc883b24d896db05a
Lupich0322 <huajibupi@gmail.com>
2023-07-04 00:14:57 +0800

    后端登录注册

---
+++ src/test/java/Test.java
@@ -0,0 +1,19 @@
+import java.io.File;
+
+public class Test {
+    public static void main(String[] args) {
+        File dir = new File("D:\\A-workspace\\CRMManagementSystem");  // 替换为你需要查看的目录路径
+        printDirectoryTree(dir, "");
+    }
+
+    public static void printDirectoryTree(File dir, String indent) {
+        if (dir.isDirectory()) {
+            System.out.println(indent + "[D] " + dir.getName());
+            for (File file : dir.listFiles()) {
+                printDirectoryTree(file, indent + "  ");
+            }
+        } else if (dir.isFile()) {
+            System.out.println(indent + "[F] " + dir.getName());
+        }
+    }
+}
